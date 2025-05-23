4a2e0c46095337de5d9680f2a376d97f4907d6fd
Brett <brettddevore@gmail.com>
2025-05-23 15:41:51 -0500

    Add Material UI theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider, createTheme } from '@mui/material'
 import './index.css'
 import App from './App.tsx'
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4285F4',
+    },
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
